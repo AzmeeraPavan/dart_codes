b1ea72e75c72516039cc2ad41ee5d66bafe6598e
Pavan Azmeera / SID Global <pavanaz@sidgs.com>
2025-09-01 11:27:52 +0530

    Added Dart OOP code

--- main.dart
+++ main.dart
@@ -1,4 +1,9 @@
 void main() {
+  List<int> l = [10, 30, 20, 40, 50, 12, 23, 15, 13];
+  print("Unsorted List $l");
+
+  l.sort();
+  print("sorted List $l");
   Set<int> s = {10, 20, 30, 40, 50, 80, 6, 5, 1, 2, 3, 4};
   print(s);
   s.length;
