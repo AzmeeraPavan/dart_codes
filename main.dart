ea5da5fb556620069e3b91be500ab1e06b15752a
Pavan Azmeera / SID Global <pavanaz@sidgs.com>
2025-09-01 11:16:34 +0530

    Added Dart OOP code

---
+++ main.dart
@@ -0,0 +1,10 @@
+void main() {
+  Set<int> s = {10, 20, 30, 40, 50, 80, 6, 5, 1, 2, 3, 4};
+  print(s);
+  s.length;
+  print(s.length);
+  List<int> s1 = s.toList();
+  print(s1);
+  s1.sort();
+  print(s1);
+}
